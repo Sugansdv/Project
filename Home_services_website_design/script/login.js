d57fc2a889b84f670810f00aec01a6c9bb592498
Sugansdv <suganyasdv16@gmail.com>
2025-05-19 13:56:07 +0530

    chnges

--- Home_services_website_design/script/login.js
+++ Home_services_website_design/script/login.js
@@ -166,3 +166,12 @@ function checkLoginStatus() {
     }
   }
 }
+
+function togglePassword(icon, inputId) {
+  const input = document.getElementById(inputId);
+  const isPassword = input.type === "password";
+
+  input.type = isPassword ? "text" : "password";
+  icon.classList.toggle("bi-eye");
+  icon.classList.toggle("bi-eye-slash");
+}
